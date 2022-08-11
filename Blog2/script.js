38fd9f2e1b0943704a98fbcedbe40949d069259d
shivam <shivamrajpoot3703@gmail.com>
2022-08-11 14:22:01 +0530

    added

---
+++ Blog2/script.js
@@ -0,0 +1,95 @@
+// Follow naming convention while defining variable names:
+
+// Define 5 variables and store different string values
+/*let fristName = "shivam"
+let lastName = "ajay";
+let username = "himanshu";
+let str = "pushpendra";
+let name = "gagan";
+*/
+
+// Define 5 variable and store different number values
+/*let num = 23;
+let number = 25;
+let lastValue =99;
+let firstValue =1;
+let middekValue =4;
+*/
+
+// Define 10 variables and store any kind of value in it. It should cover at least one value of each data type.
+/*let fristName = "shivam"
+let lastName = "ajay";
+let username = "himanshu";
+let str = "pushpendra";
+let name = "gagan";
+let num = 23;
+let number = 25;
+let lastValue =99;
+let firstValue =1;
+{username:"shivam", age:22}
+*/
+
+
+// Declare age and gender in the same line as userName i.e. declare multiple variables in one line. (let a = 21, b = 22;)
+// age:22, gender:"male", username:"shivam" ;
+
+
+// Declare variables with names like break, class, const, for, else, if, import and return. Explain in your own words what did you observe.
+
+// Write valid and invalid along with the reason next to each line of the code given below:
+
+// let monk;
+// valid
+
+
+// let 1stName;
+// invalid
+
+
+// let places3;
+// valid
+
+
+// let -name;
+// invalid
+
+// let bigPanther;
+// invalid
+
+
+// let 35;
+// invalid
+
+
+// let 43 = 30;
+// invalid
+
+
+// let $48;
+// valid
+
+// var *gt = 350;
+// invalid
+
+// let userName;
+// valid
+
+// let a, b, c;
+// valid
+
+// let x = 3, y = 11, z = 13;
+// valid
+
+
+// let x = 2 + 5 + 7;
+// valid
+
+// let user = "Brienne of Tarth"; "Sansa Stark"; "Lyanna Mormont";
+// valid
+
+
+// What message will be printed in console?
+// let charactor = "Arya";
+// console.log(typeof charactor);    / string
+// charactor = 10;
+
